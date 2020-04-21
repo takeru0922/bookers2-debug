091d0ebbfde879595ee27128b22ae8e13a08b220
takeru0922 <moriai0922takeru@gmail.com>
2020-04-21 18:05:12 +0900

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
         root_path
     end
 
-    def configure_permitted_parameters¥
+    def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
     end
 end
